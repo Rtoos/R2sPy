73b73ecc5d81550c1def920ed8f846ab735a9611
mlbernardoni <mike@bernardoni.biz>
2021-05-09 10:09:50 -0500

    included readme and changes to make it more readable

--- tests.py
+++ tests.py
@@ -6,7 +6,7 @@ import datetime
 
 file = open('tests.csv', 'w')
 rtoossend = {}
-x = requests.post('http://R2stestapp-env.eba-txcmd3gh.us-east-2.elasticbeanstalk.com/Tests.html', json = rtoossend)
+x = requests.post('http://localhost:8080/R2FileApp/Tests.html', json = rtoossend)
 testsarray = json.loads(x.text)
 for raw in testsarray:
     #print(raw)
@@ -15,20 +15,18 @@ for raw in testsarray:
     authenticate = row['authenticate']
     clear = row['clear']
     starttimestr = row['starttime']
-    starttimestr2 = starttimestr.rstrip(starttimestr[-1])
-    element = datetime.datetime.strptime(starttimestr2, "%Y-%m-%d %H:%M:%S.%f")
+    starttimestr = starttimestr.rstrip(starttimestr[-1])
+    element = datetime.datetime.strptime(starttimestr, "%Y-%m-%d %H:%M:%S.%f")
     starttime = datetime.datetime.timestamp(element)
     endtimestr = row['endtime']
-    diff = 0
-    if endtimestr != None :
-        endtimestr = endtimestr.rstrip(endtimestr[-1])
-        element = datetime.datetime.strptime(endtimestr, "%Y-%m-%d %H:%M:%S.%f")
-        endtime = datetime.datetime.timestamp(element)
-        diff = endtime - starttime
+    endtimestr = endtimestr.rstrip(endtimestr[-1])
+    element = datetime.datetime.strptime(endtimestr, "%Y-%m-%d %H:%M:%S.%f")
+    endtime = datetime.datetime.timestamp(element)
+    diff = endtime - starttime
     #seconds = diff.total_seconds()
     transactions = row['transactions']
     type = row['type']
-    strcsv = file_id + ',' + starttimestr + ',' + type + ',' + authenticate + ',' + clear + ',' + str(transactions) + ',' + str(diff) + '\n'
+    strcsv = file_id + ',' + type + ',' + authenticate + ',' + clear + ',' + str(transactions) + ',' + str(diff) + '\n'
     file.write(strcsv)
     #print(strcsv)
 
